a389402903d8514cbbc5614545ea082e8196e4a1
Daniel VIllarroel <daniel.villarroel.m@usach.cl>
2025-12-02 22:03:46 -0300

    arreglo

---
+++ tingeso_backend/src/test/java/com/example/tingeso_backend/TingesoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tingeso_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TingesoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
