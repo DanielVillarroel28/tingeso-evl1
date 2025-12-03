7de3609684955747e770e6286e90f87a9069d289
Daniel VIllarroel <daniel.villarroel.m@usach.cl>
2025-12-03 01:57:41 -0300

    editado 2

--- tingeso_frontend/tingeso_frontend/src/components/Home.jsx
+++ tingeso_frontend/tingeso_frontend/src/components/Home.jsx
@@ -3,7 +3,7 @@ const Home = () => {
     <div>
       <h1>ToolRent: Tu herramienta definitiva</h1>
       <p>
-        Bienvenido a ToolRent, la plataforma líder en alquiler de herramientas para todas tus necesidades. Ya sea que estés buscando herramientas para proyectos de bricolaje, construcción o jardinería, tenemos todo lo que necesitas.
+        Bienvenido a ToolRentkkkk, la plataforma líder en alquiler de herramientas para todas tus necesidades. Ya sea que estés buscando herramientas para proyectos de bricolaje, construcción o jardinería, tenemos todo lo que necesitas.
       </p>
     </div>
     
